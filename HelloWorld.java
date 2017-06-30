17a68304687239c7634966f500aa88e4c2094b00
benjamas1 <thainovel2@yahoo.com>
2017-06-30 14:40:10 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ class HelloWorld {
 	public static void main(String [] args) {
 		System.out.print("Hello from git to jenkins");
 		System.out.print("BYEEEEEEEEEEEEEEEEEEEEE");
+		system.out.print("bye2 );
 					}
 }

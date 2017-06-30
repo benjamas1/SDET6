83624c78e6749506920d344fc81beff7fed723e7
benjamas1 <thainovel2@yahoo.com>
2017-06-30 14:33:55 -0400

    change2

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ package com.fannie.gitproj;
 class HelloWorld {
 	public static void main(String [] args) {
 		System.out.print("Hello from git to jenkins");
-	}
+		System.out.print("BYEEEEEEEEEEEEEEEEEEEEE");
+					}
 }
